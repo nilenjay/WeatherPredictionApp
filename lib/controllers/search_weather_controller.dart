2f704f0834440f83cd00f0c1d8d26b3b9ad20fc0
nilenjay <nilenjaysinghsengar2006@gmail.com>
2025-10-24 01:28:38 +0530

    Search Screen UI

---
+++ lib/controllers/search_weather_controller.dart
@@ -0,0 +1,36 @@
+// lib/controllers/search_weather_controller.dart
+import 'package:get/get.dart';
+import '../models/weather_model.dart';
+import '../services/search_weather_service.dart';
+
+class SearchWeatherController extends GetxController {
+  final WeatherService _weatherService = WeatherService();
+
+  var weatherData = Rx<WeatherModel?>(null);
+  var isLoading = false.obs;
+  var errorMessage = ''.obs;
+
+  /// Fetch weather for a given city name
+  Future<void> fetchWeatherForCity(String cityName) async {
+    try {
+      isLoading.value = true;
+      errorMessage.value = '';
+      weatherData.value = null;
+
+      final WeatherModel weather = await _weatherService.fetchWeatherByCity(cityName);
+
+      weatherData.value = weather;
+    } catch (e) {
+      errorMessage.value = e.toString();
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  /// Clear previous search
+  void clear() {
+    weatherData.value = null;
+    errorMessage.value = '';
+    isLoading.value = false;
+  }
+}
